a69f5af2e95e4feede1969a98c5ba54fea88f064
Siddharth Manghwani <sid.m2508@gmail.com>
2021-05-30 02:05:37 +0530

    Basic Phrases App

--- app/src/main/java/com/siddharth/practice/MainActivity.java
+++ app/src/main/java/com/siddharth/practice/MainActivity.java
@@ -6,36 +6,63 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-public void playAudio(View view){
+    ListView listView;
+    public void tableCreate(int number){
 
-    int btnid = view.getId();
-
-    String buttonID = view.getResources().getResourceEntryName(btnid);
-
-    int resourceId = view.getResources().getIdentifier(buttonID , "raw" , "com.siddharth.practice");
-
-    MediaPlayer mediaPlayer = MediaPlayer.create(this , resourceId);
-    mediaPlayer.start();
-}
+        ArrayList<String> arrayList = new ArrayList<String>();
+        for(int a = 1 ; a<=10 ; a++)
+        {
+            arrayList.add(Integer.toString(number * a));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this , android.R.layout.simple_list_item_1 , arrayList);
+        listView.setAdapter(adapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setMax(20);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tableCreate(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }
